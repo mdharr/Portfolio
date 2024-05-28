21e77f48345ffc21333eabdeb5ddaffb1616c9d1
Michael Harrington <mdharr7@gmail.com>
2024-05-28 15:21:21 +0800

    Add styles and template boilerplate to portfolio site.

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const welcomeHeader = document.getElementById('welcome-header');
-const revealBtn = document.getElementById('reveal-btn');
-
-function reveal() {
-    const h2Element = document.createElement('h2');
-    h2Element.textContent = "Michael Harrington's Portfolio";
-    h2Element.classList.add('colorful');
-    welcomeHeader.appendChild(h2Element);
-    revealBtn.remove();
+function toggleMobileMenu(){
+	document.getElementById("menu").classList.toggle("active");
 }
-
-revealBtn.addEventListener('click', reveal);
